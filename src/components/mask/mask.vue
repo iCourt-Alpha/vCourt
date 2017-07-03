e83dfc2b8a215c7cfc2c859132a440244bfcbd89
suChang <313439271@qq.com>
2017-07-03 16:22:29 +0800

    mask组件 修改

--- src/components/mask/mask.vue
+++ src/components/mask/mask.vue
@@ -7,63 +7,57 @@
 </template>
 
 <script type='text/ecmascript-6'>
-import { dom } from '../../utils/dom'
-export default {
-  props: {
-    maskShow: {
-      type: Boolean,
-      default: false
-    },
-    bkg: {
-      type: String,
-      default: 'dark'
-    }
-  },
-  methods: {
-    maskClick (e) {
-      if (e.target === this.$el) {
-        this.$emit('maskClicked')
+  import { dom } from '../../utils/dom'
+  export default {
+    props: {
+      maskShow: {
+        type: Boolean,
+        default: false
+      },
+      bkg: {
+        type: String,
+        default: 'dark'
       }
     },
-    afterEnter () {
-      this.$emit('afterEnter')
-    }
-  },
-  watch: {
-    maskShow (n, o) {
-      if (typeof n === 'boolean' && typeof o === 'boolean' && n === false) {
-        let that = this
-        setTimeout(function () {
-          that.$destroy()
-        })
+    methods: {
+      maskClick (e) {
+        if (e.target === this.$el) {
+          this.$emit('maskClicked')
+        }
+      },
+      afterEnter () {
+        this.$emit('afterEnter')
       }
+    },
+    watch: {
+      maskShow (n, o) {
+        if (typeof n === 'boolean' && typeof o === 'boolean' && n === false) {
+          dom.setStyle(document.body, 'overflow', this._curOverflowStyle)
+          this._curOverflowStyle = null
+        }
+      }
+    },
+    mounted () {
+      this._curOverflowStyle = dom.getStyle(document.body, 'overflow')
+      dom.setStyle(document.body, 'overflow', 'hidden')
     }
-  },
-  mounted () {
-    this._curOverflowStyle = dom.getStyle(document.body, 'overflow')
-    dom.setStyle(document.body, 'overflow', 'hidden')
-  },
-  destroyed () {
-    dom.setStyle(document.body, 'overflow', this._curOverflowStyle)
-    this._curOverflowStyle = null
   }
-}
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus'>
-@import "../../stylus/customize.styl"
-@import "../../stylus/animation.styl"
-@import "../../stylus/constant.styl"
+  @import "../../stylus/customize.styl"
+  @import "../../stylus/animation.styl"
+  @import "../../stylus/constant.styl"
 
-.vc-mask
-  position: fixed
-  top: 0
-  left: 0
-  bottom: 0
-  right: 0
-  z-index: maskZ
-.vc-mask-dark
-  background maskBkgColor
-.iui-mask-transparent
-  background rgba(0,0,0,0)
+  .vc-mask
+    position: fixed
+    top: 0
+    left: 0
+    bottom: 0
+    right: 0
+    z-index: maskZ
+  .vc-mask-dark
+    background maskBkgColor
+  .vc-mask-transparent
+    background rgba(0,0,0,0)
 </style>
